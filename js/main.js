5ee8a2326585b846c08a876e01c0c1b855c701af
Tom Lee <tomtleee@gmail.com>
2020-07-24 20:27:25 -0400

    Start on tabbed bar charts

--- js/main.js
+++ js/main.js
@@ -781,6 +781,27 @@ require([
 
         return formattedName;
     };
-})
+});
+
+function openChart(evt, tabName) {
+    // Declare all variables
+    var i, tabContent, tabLinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabContent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tabLinks = document.getElementsByClassName("tab-ink");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+};
 
 
